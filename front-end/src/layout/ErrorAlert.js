213a74803bc11d4cecaa0a9bd8a24192eb5dd9c0
Chris Ibarra <chrisaaron184@gmail.com>
2023-10-30 15:41:32 -0700

    Updated ErrorAlert With Close Button

--- front-end/src/layout/ErrorAlert.js
+++ front-end/src/layout/ErrorAlert.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 /**
  * Defines the alert message to render if the specified error is truthy.
  * @param error
@@ -8,12 +7,25 @@ import React from "react";
  *  a bootstrap danger alert that contains the message string.
  */
 
-function ErrorAlert({ error }) {
+function ErrorAlert({ error, setError }) {
   return (
     error && (
-      <div className="alert alert-danger m-2">Error: {error.message}</div>
+      <div 
+        className="alert alert-danger m-2 alert-dismissible fade show"
+        role="alert"
+      >
+        <strong>Error</strong>: {error.message}
+        <button
+          type="button"
+          className="close"
+          data-dismiss="alert"
+          aria-label="Close"
+          onClick={() => setError(false)}
+        >
+        <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
     )
   );
 }
-
 export default ErrorAlert;
