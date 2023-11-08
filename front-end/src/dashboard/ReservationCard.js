d8e081c01aa4382e117b1ec9565de1249b8b2dc2
Chris Ibarra <chrisaaron184@gmail.com>
2023-11-08 15:47:22 -0800

    Added "Seat" Button

--- front-end/src/dashboard/ReservationCard.js
+++ front-end/src/dashboard/ReservationCard.js
@@ -1,15 +1,14 @@
 import React from "react";
-
 function ReservationCard({ 
-    reservation_id, 
-    first_name, 
-    last_name, 
-    mobile_number, 
-    reservation_date, 
-    reservation_time, 
-    people 
-  }) {
-
+  reservation_id, 
+  first_name, 
+  last_name, 
+  mobile_number, 
+  reservation_date, 
+  reservation_time, 
+  people 
+}) {
+    
   return (
     <div className="card border-secondary mb-3" style={{maxWidth: "25rem"}}>
       <h4 className="card-header">{last_name}, {first_name}</h4>
@@ -19,9 +18,17 @@ function ReservationCard({
         <h6 className="card-subtitle mb-2 text-muted">Mobile Number: {mobile_number}</h6>
       </div>
       <div 
-        className="card-footer border-secondary text-secondary"
-        style={{textAlign: "right"}} >
-        Reservation #: {reservation_id}
+        className="card-footer border-secondary text-secondary d-inline-flex justify-content-between align-items-center"
+        style={{textAlign: "right"}} 
+      >
+      <a 
+        className="btn btn-secondary" 
+        href={`/reservations/${reservation_id}/seat`} 
+        role="button"
+      >
+        Seat
+      </a>
+      Reservation #{reservation_id}
       </div>
     </div>
   );
