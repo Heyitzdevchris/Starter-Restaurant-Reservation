b21af9c54b1945d366daa79ac69d386b5792da8f
Chris Ibarra <chrisaaron184@gmail.com>
2023-11-08 15:45:04 -0800

    Added Create Table Function

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -1,11 +1,18 @@
 const knex = require("../db/connection");
-
 function list() {
   return knex("tables")
     .select("*")
     .orderBy("table_name");
 }
 
+function create(newTable) {
+  return knex("tables")
+    .insert(newTable)
+    .returning("*")
+    .then(createdRecords => createdRecords[0]);
+}
+
 module.exports = {
+    create,
     list,
 }
