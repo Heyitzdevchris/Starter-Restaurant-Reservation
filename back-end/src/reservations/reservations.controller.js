79693ff22c95d427bed69ef23818be9a847d1190
Chris Ibarra <chrisaaron184@gmail.com>
2023-11-08 16:13:23 -0800

    reservationExists & Read Reservation Functions

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -30,6 +30,7 @@ function hasData(req, res, next) {
   }
   next({ status: 400, message: "Body must have data property"});
 }
+
 function hasOnlyValidProperties(req, res, next) {
   const { data = {} } = req.body;
   const invalidProperties = Object.keys(data).filter(
@@ -40,6 +41,7 @@ function hasOnlyValidProperties(req, res, next) {
   }
   next();
 }
+
 function hasProperties(...properties) {
   return function (req, res, next) {
     const { data = {} } = req.body;
@@ -57,6 +59,7 @@ function hasProperties(...properties) {
     }
   };
 }
+
 function hasValidDate(req, res, next) {
   const { data: { reservation_date } = {} } = req.body;
   const dateRegex = new RegExp(/(?<=\D|^)(?<year>\d{4})(?<sep>[^\w\s])(?<month>1[0-2]|0[1-9])\k<sep>(?<day>0[1-9]|[12][0-9]|(?<=11\k<sep>|[^1][4-9]\k<sep>)30|(?<=1[02]\k<sep>|[^1][13578]\k<sep>)3[01])(?=\D|$)/gm);
@@ -65,6 +68,7 @@ function hasValidDate(req, res, next) {
   }
   next();
 }
+
 function hasValidTime(req, res, next) {
   const { reservation_time } = req.body.data;
   const timeRegex = new RegExp(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/);
@@ -74,6 +78,7 @@ function hasValidTime(req, res, next) {
     next({ status: 400, message: "reservation_time must be a valid time"});
   }
 }
+
 function peopleIsNumber(req, res, next) {
   const { data: { people } = {} } = req.body;
   if (!Number.isInteger(people)) {
@@ -81,6 +86,7 @@ function peopleIsNumber(req, res, next) {
   }
   next();
 }
+
 function IsNotTuesday(req, res, next) {
   const { reservation_date } = req.body.data;
   const dateString = reservation_date.split("-");
@@ -98,6 +104,7 @@ function IsNotTuesday(req, res, next) {
     next();
   }
 }
+
 function IsNotPastDate(req, res, next) {
   const { reservation_date, reservation_time } = req.body.data;
   const [hour, minute] = reservation_time.split(":");
@@ -126,7 +133,6 @@ function isWithinBusinessHours(req, res, next) {
     });
   }
 }
-
 /**
  * Create new reservation handler
  */
@@ -135,6 +141,29 @@ async function create(req, res) {
   console.log("data", data);
   res.status(201).json({ data: data });
 }
+
+/**
+ * Check that given reservation exists
+ */
+async function reservationExists(req, res, next) {
+  const reservation_id = req.params.reservation_id;
+  const reservation = await service.read(reservation_id);
+  if (reservation) {
+    res.locals.reservation = reservation;
+    return next();
+  }
+  next({ status: 404, message: `reservation ${reservation_id} does not exist` });
+}
+
+/**
+ * Read given reservation by ID
+ */
+function read(req, res, next) {
+  const data = res.locals.reservation;
+  res.json({ data });
+}
+
+
 module.exports = {
   create: [
     hasData,
@@ -149,4 +178,8 @@ module.exports = {
     asyncErrorBoundary(create),
   ],
   list: asyncErrorBoundary(list),
+  read: [
+    asyncErrorBoundary(reservationExists),
+    read,
+  ],
 };
