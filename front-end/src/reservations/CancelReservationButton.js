bd7dc66361828ebc5643961d6f5dc5b017f9f77c
Chris Ibarra <chrisaaron184@gmail.com>
2023-11-14 05:11:23 -0800

    Have to Use window.confirm Instead of Modal For Now For Tests to Pass

--- front-end/src/reservations/CancelReservationButton.js
+++ front-end/src/reservations/CancelReservationButton.js
@@ -3,19 +3,22 @@ import { Button, Modal } from "react-bootstrap";
 import { cancelReservation } from "../utils/api";
 
 function CancelReservationButton({ reservation_id, setReservationsError, loadReservationsAndTables }) {
-
+  
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const handleOk = (event) => {
     event.preventDefault();
-    cancelReservation(reservation_id, "cancelled")
-      .then(() => loadReservationsAndTables())
-      .then(handleClose)
-      .catch(setReservationsError);
-  }
+    // Want to use modal - come back to this later. //
+    const message = "Do you want to cancel this reservation? This cannot be undone.";
+    if (window.confirm(message)) {
+      cancelReservation(reservation_id, "cancelled")
+        .then(() => loadReservationsAndTables())
+        // .then(handleClose)
+        .catch(setReservationsError);
+    }
+  };
 
 
   return (
@@ -23,12 +26,12 @@ function CancelReservationButton({ reservation_id, setReservationsError, loadRes
       <button
         type="button"
         className="btn btn-danger"
-        onClick={handleShow}
+        // Use handleShow here for modal
+        onClick={handleOk}
         data-reservation-id-cancel={reservation_id}
       >
         Cancel
       </button>
-
       {/* Modal */}
       <Modal show={show} onHide={handleClose}>
         <Modal.Header>
@@ -47,4 +50,6 @@ function CancelReservationButton({ reservation_id, setReservationsError, loadRes
     </>
   );
 }
+
+
 export default CancelReservationButton;
