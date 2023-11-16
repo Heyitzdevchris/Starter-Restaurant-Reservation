52f16eae2ac8ab6b108cf9fced72a9d10648d9f3
Chris Ibarra <chrisaaron184@gmail.com>
2023-11-15 17:32:27 -0800

    Added PUT Method For Update

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -8,10 +8,12 @@ router.route("/:reservation_id/status")
 
 router.route("/:reservation_id")
   .get(controller.read)
+  .put(controller.update)
   .all(methodNotAllowed);
-  
+
 router.route("/")
   .get(controller.list)
   .post(controller.create)
   .all(methodNotAllowed);
+
 module.exports = router;
