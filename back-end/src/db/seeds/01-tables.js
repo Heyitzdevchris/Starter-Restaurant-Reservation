608ed11fdfd844ac1877ecee77d70d2983016429
Chris Ibarra <chrisaaron184@gmail.com>
2023-11-23 03:33:06 -0800

    Adjusted then() chain

--- back-end/src/db/seeds/01-tables.js
+++ back-end/src/db/seeds/01-tables.js
@@ -1,8 +1,7 @@
 const tables = require("./01-tables.json");
 
 exports.seed = function (knex) {
-  return knex.raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
-    .then(function () {
-      return knex("tables").insert(tables);
-    });
+  return knex
+    .raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
+    .then(() => knex("tables").insert(tables));
 };
